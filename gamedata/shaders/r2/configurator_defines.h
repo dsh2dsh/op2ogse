8131d5408d58eb54184a6aafded69d9e141d2a53
Denis Shaposhnikov <dsh@bamus.cz>
2016-02-16 16:50:16 +0100

    OGSE 1.12

--- gamedata/shaders/r2/configurator_defines.h
+++ gamedata/shaders/r2/configurator_defines.h
@@ -5,14 +5,14 @@
 #define USE_MOON_ROAD
 #define FL_USE_LONG_FLARE
 //#define SSDO_USE_INDIRECT_BOUNCES
-#define DDOF_VIGNETTING
-#define DDOF_CHROMATIC_ABERRATION
+//#define DDOF_VIGNETTING
+//#define DDOF_CHROMATIC_ABERRATION
 
 #define SS_VOL_MAX_INTENSITY float(0.100)
 #define VOL_LIGHT_MAX_INTENSITY float(0.015)
 #define SSDO_BLEND_FACTOR float(1.500)
 #define HBAO_BLEND_FACTOR float(0.700)
-#define DDOF_VIGNOUT float(0.000)
+#define DDOF_VIGNOUT float(0.020)
 #define DDOF_GAIN float(1.000)
 #define ZDOF_INTENSITY float(0.600)
 #define RDOF_INTENSITY float(0.400)
