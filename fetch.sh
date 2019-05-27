fa73490f95780ed76a09bd5cfecd744ca50c2cc9
Denis Shaposhnikov <dsh@bamus.cz>
2019-05-27 11:58:35 +0200

    Распакованный shaders_xrlc.xr

--- fetch.sh
+++ fetch.sh
@@ -8,4 +8,5 @@ rsync -aFv --no-g --no-p --delete --filter=':- .gitignore' \
       ${ROOT}/gamemtl \
       ${ROOT}/ReadMe_dsh.txt \
       ${ROOT}/shaders_xr \
+      ${ROOT}/shaders_xrlc_xr \
       ./
