18024e972ef14a336236a93f29896f4fb1f9b7b6
Krodin <16670637+KRodinn@users.noreply.github.com>
2020-05-01 18:02:09 +0200

    Выключил FXAA_GREEN_AS_LUMA на R3 и R4

--- gamedata/shaders/r3/fxaa.s
+++ gamedata/shaders/r3/fxaa.s
@@ -1,7 +1,14 @@
 function element_0(shader, t_base, t_second, t_detail)
-	shader:begin("fxaa_main", "fxaa_main")
+	shader:begin("fxaa_luma", "fxaa_luma")
 		:fog(false)
 		:zb(false, false)
 	shader:dx10texture("s_base0", "$user$generic0")
+end
+
+function element_1(shader, t_base, t_second, t_detail)
+	shader:begin("fxaa_main", "fxaa_main")
+		:fog(false)
+		:zb(false, false)
+	shader:dx10texture("s_base0", "$user$generic1")
 	shader:dx10sampler("smp_rtlinear")
 end
