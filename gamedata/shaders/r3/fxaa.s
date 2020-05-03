37339619e9a508ef873f96c209a9d1e5fbb8731b
Denis Shaposhnikov <993498+dsh2dsh@users.noreply.github.com>
2020-05-03 12:20:36 +0200

    Добавил несколько вариантов сглаживания
    FXAA1 - то же самое, что было изначально
    
    FXAA2 - FXAA с новыми настройками из https://github.com/xrOxygen/xray-oxygen
    
    DLAA  - DLAA из https://github.com/ForserX/DLAA

--- gamedata/shaders/r3/fxaa.s
+++ gamedata/shaders/r3/fxaa.s
@@ -12,3 +12,12 @@ function element_1(shader, t_base, t_second, t_detail)
 	shader:dx10texture("s_base0", "$user$generic1")
 	shader:dx10sampler("smp_rtlinear")
 end
+
+function element_2(shader, t_base, t_second, t_detail)
+	shader:begin("taa_main", "taa_main")
+		:fog(false)
+		:zb(false, false)
+	shader:dx10texture("s_image", "$user$generic0")
+	shader:dx10texture("s_position", "$user$position")
+	shader:dx10sampler("smp_rtlinear")
+end
