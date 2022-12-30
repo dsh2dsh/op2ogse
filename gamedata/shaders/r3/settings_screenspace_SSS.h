071cf53c584c4ab956f20bb6d6496a0b4abc8ca3
Denis Shaposhnikov <993498+dsh2dsh@users.noreply.github.com>
2022-12-30 15:17:36 +0100

    Отключил на всякий случай в новых шейдерах G_SSDO_(GRASS|WEAPON)_HARDNESS
    Не уверен, что это и нужно-ли это вообще.

--- gamedata/shaders/r3/settings_screenspace_SSS.h
+++ gamedata/shaders/r3/settings_screenspace_SSS.h
@@ -9,9 +9,9 @@
 #define G_SSDO_SCENARY_HARDNESS 1.0f // Scenary shadow hardness. 0.0f to disable scenary shadows.
 #define G_SSDO_SCENARY_SHADOW_LENGTH 1.5f // Scenary maximum shadow length.
 
-#define G_SSDO_GRASS_HARDNESS 1.0f // Grass shadow hardness.
+#define G_SSDO_GRASS_HARDNESS 0.f //dsh: 1.0f // Grass shadow hardness.
 #define G_SSDO_GRASS_SHADOW_LENGTH 0.5f // Grass maximum shadow length.
 
 #define G_SSDO_WEAPON_LENGTH 1.5f // Maximum distance to apply weapon factors.
-#define G_SSDO_WEAPON_HARDNESS 1.0f // Weapon shadow hardness. 0.0f to disable weapon shadows.
+#define G_SSDO_WEAPON_HARDNESS 0.f //dsh: 1.0f // Weapon shadow hardness. 0.0f to disable weapon shadows.
 #define G_SSDO_WEAPON_SHADOW_LENGTH 0.09f // Weapon maximum shadow length.
